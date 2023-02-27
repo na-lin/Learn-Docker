5c0410fc94d0495ed324af80c9875a087ecc28f2
Na Lin <lna483018@gmail.com>
2023-02-27 14:35:34 -0500

    update source code

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi there from Docker.");
+  res.send("Good Bye docker.");
 });
 
 app.listen(8080, () => {
