f4d12a616be53ecbe656a092aeb6b73058e26ac4
Na Lin <lna483018@gmail.com>
2023-03-02 21:43:18 -0500

    connect to redis server in other container

--- visits/index.js
+++ visits/index.js
@@ -4,7 +4,11 @@ const redis = require("redis");
 const app = express();
 
 // Connect to redis server
-const client = redis.createClient();
+// location of the redis server where we are running
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+});
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
